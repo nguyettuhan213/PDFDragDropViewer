890e723cfe95d9dc9a224c074778507fd28c2e9a
nguyettuhan213 <117661080+nguyettuhan213@users.noreply.github.com>
2025-06-09 14:46:34 +0700

    add /generated

---
+++ PDFDragDropViewer/generated/ManifestTypes.d.ts
@@ -0,0 +1,21 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    sampleProperty: ComponentFramework.PropertyTypes.StringProperty;
+    InFilePDFContent: ComponentFramework.PropertyTypes.StringProperty;
+    InFileImageContent: ComponentFramework.PropertyTypes.StringProperty;
+    XLocation: ComponentFramework.PropertyTypes.StringProperty;
+    YLocation: ComponentFramework.PropertyTypes.StringProperty;
+    PageNumber: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    sampleProperty?: string;
+    InFilePDFContent?: string;
+    InFileImageContent?: string;
+    XLocation?: string;
+    YLocation?: string;
+    PageNumber?: string;
+}
